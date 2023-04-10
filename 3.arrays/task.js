52b079d5c570ac22b4fe9ac10cc61e5eedbc8b8e
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2023-04-10 16:20:23 +0300

    Третье задание полностью выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+    let compare = (arr1, arr2) => {
+        return arr1.length === arr2.length &&
+        arr1.every((element, index) => element === arr2[index])
+    }
+    return compare(arr1, arr2)
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if(users.length == 0 || gender == "инопланетянин") {
+        return 0
+    }
+    let arr = users.filter(item => item.gender == gender).map(item => item.age);
+    let result = arr.reduce((a, b) => a + b)  
+    result = result/arr.length
+    return result
 }
